3c6ac6f7c3ab14147a1266febd0115e4d6f35b31
reric88 <ruffyreborn@gmail.com>
2023-08-17 17:47:38 -0500

    Added additional components

---
+++ src/components/Status.tsx
@@ -0,0 +1,11 @@
+
+
+export const Status = () => {
+  return (
+    <div>
+        <h2>Loading...</h2>
+        <h2>Data fetched successfully!</h2>
+        <h2>Error fetching data</h2>
+    </div>
+  )
+}
---
+++ src/components/Validation.tsx
@@ -0,0 +1,7 @@
+
+
+export const Validation = () => {
+  return (
+    <div>Validation</div>
+  )
+}
