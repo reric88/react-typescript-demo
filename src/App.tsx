c42cc92e3da643e9bcdd51cdb0de6d17ffd5e681
reric88 <ruffyreborn@gmail.com>
2023-08-17 19:44:17 -0500

    Added usestates and useeffects to prevent names and values from updating randomly without being called.

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,152 @@
-
-import { useEffect, useState } from 'react';
-import './App.css';
-import { Greet } from './components/Greet';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
-import { Status } from './components/Status';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
+import { Status } from "./components/Status";
 
 function App() {
-  const [pageStatus, setPageStatus] = useState('loading')
+  const [randName, setRandName] = useState("");
+  const [fullRandName, setFullRandName] = useState({
+    first: "",
+    last: "",
+  });
+  const [nameList, setNameList] = useState([
+    {
+      first: "",
+      last: "",
+    },
+  ]);
+  const [messageNum, setMessageNum] = useState(0);
+  const [pageStatus, setPageStatus] = useState("loading");
+  const [pageReason, setPageReason] = useState<
+    "loading" | "success" | "failure" | "fatal"
+  >("loading");
+
   const personName = {
-    first: 'Bruce',
-    last: 'Wayne',
-  }
-  const firstNames = ['Eric', 'Vishwas', 'John', 'Bruce', 'Jason', 'Sam', 'Andy', 'Luke', 'Clark', 'Lois', 'Leia', 'Ben', 'Kevin', 'Chad', 'Brad', 'Larry', 'Brenda', 'Lane', 'Trisha', 'Sarah', 'Sal']
-  const lastNames = ['Collard', 'Sapupu', 'Jacobs', 'Wayne', 'Smith', 'Cook', 'Perry', 'Fields', 'Skywalker', 'Kent', 'Baldor', 'Priss', 'Stevens', 'Sanders', 'Jackson', 'Stiles', 'Harrison', 'Garry', 'Bland', 'Pride', 'Mavik']
-  const messageCount = Math.floor(Math.random() * 11)
+    first: "Bruce",
+    last: "Wayne",
+  };
 
+  const firstNames = [
+    "Eric",
+    "Vishwas",
+    "John",
+    "Bruce",
+    "Jason",
+    "Sam",
+    "Andy",
+    "Luke",
+    "Clark",
+    "Lois",
+    "Leia",
+    "Ben",
+    "Kevin",
+    "Chad",
+    "Brad",
+    "Larry",
+    "Brenda",
+    "Lane",
+    "Trisha",
+    "Sarah",
+    "Sal",
+  ];
 
-  const randomName = () =>{
-    const names = {
-      first: '',
-      last: '',
+  const lastNames = [
+    "Collard",
+    "Sapupu",
+    "Jacobs",
+    "Wayne",
+    "Smith",
+    "Cook",
+    "Perry",
+    "Fields",
+    "Skywalker",
+    "Kent",
+    "Baldor",
+    "Priss",
+    "Stevens",
+    "Sanders",
+    "Jackson",
+    "Stiles",
+    "Harrison",
+    "Garry",
+    "Bland",
+    "Pride",
+    "Mavik",
+  ];
+
+  useEffect(() => {
+    let results = ["success", "failure", "fatal"];
+    setTimeout(() => {
+      const randomStatus = results[Math.floor(Math.random() * 3)];
+      setPageStatus(randomStatus);
+      setPageReason(
+        randomStatus as "loading" | "success" | "failure" | "fatal"
+      );
+    }, 3000);
+
+    const messageAmt = Math.floor(Math.random() * 11);
+
+    const randomName = () => {
+      let first = firstNames[Math.floor(Math.random() * 21)];
+      setRandName(first);
     };
-    let first = firstNames[Math.floor(Math.random() * 21)]
-    return first
-  }
 
-  const randomNameObject = () =>{
-    const names = {
-      first: '',
-      last: '',
+    const randomNameObject = () => {
+      const names = {
+        first: "",
+        last: "",
+      };
+      let first = firstNames[Math.floor(Math.random() * 21)];
+      let last = lastNames[Math.floor(Math.random() * 21)];
+      names.first = first;
+      names.last = last;
+      setFullRandName(names);
     };
-    let first = firstNames[Math.floor(Math.random() * 21)]
-    let last = lastNames[Math.floor(Math.random() * 21)] 
-    names.first = first;
-    names.last = last;
-    return names
-  }
 
-  const personNameList = () => {
-    const names = {
-      first: '',
-      last: '',
+    const personNameList = () => {
+      const names = {
+        first: "",
+        last: "",
+      };
+      let first = firstNames[Math.floor(Math.random() * 21)];
+      let last = lastNames[Math.floor(Math.random() * 21)];
+      names.first = first;
+      names.last = last;
+      return names;
     };
-    let first = firstNames[Math.floor(Math.random() * 21)]
-    let last = lastNames[Math.floor(Math.random() * 21)] 
-    names.first = first;
-    names.last = last;
-    return names;
-  }
 
-  let namesList = [
-    {
-      first: personNameList().first,
-      last: personNameList().last,
-    },
-    {
-      first: personNameList().first,
-      last: personNameList().last,
-    },
-    {
-      first: personNameList().first,
-      last: personNameList().last,
-    },
+    let namesList = [
+      {
+        first: personNameList().first,
+        last: personNameList().last,
+      },
+      {
+        first: personNameList().first,
+        last: personNameList().last,
+      },
+      {
+        first: personNameList().first,
+        last: personNameList().last,
+      },
+    ];
 
-  ]
-
-useEffect(()=>{
-  let results = ['success', 'failure', 'fatal']
-  setTimeout(() => {
-    const randomStatus = results[Math.floor(Math.random() * 3)]
-    setPageStatus(randomStatus)
-  }, 3000);
-
-}, [])
+    setMessageNum(messageAmt);
+    randomName();
+    randomNameObject();
+    personNameList();
+    setNameList(namesList);
+  }, []);
 
   return (
-  <div className="App">
-    <Greet name={randomName()} messageCount={messageCount} isLoggedIn={false} />
-    <Status status={pageStatus} />
-    <Person name={personName} randomName={randomNameObject()}/>
-    <PersonList names={namesList} />
+    <div className="App">
+      <Greet name={randName} messageCount={messageNum} isLoggedIn={false} />
+      <Status status={pageStatus} reason={pageReason} />
+      <Person name={personName} randomName={fullRandName} />
+      <PersonList names={nameList} />
     </div>
-)
+  );
 }
 
 export default App;
--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,38 +1,50 @@
 type StatusProps = {
-    status: string,
-}
+  status: string,
+//   reason: "loading" | "success" | "failure" | "fatal"
+  reason: string,
+};
 
 export const Status = (props: StatusProps) => {
-    let message;
-    
-    if (props.status === 'loading'){
-        message = 'Loading...'
-    } else if (props.status === 'success'){
-        message = 'Data was fetched successfuly!'
-    } else if (props.status === 'failure'){
-        message = 'Error: Data fetch failed'
-    } else {
-        message = 'Fatal error'
-    }
+  let message;
+  let reply;
 
+  if (props.status === "loading") {
+    message = "Loading...";
+  } else if (props.status === "success") {
+    message = "Data was fetched successfuly!";
+  } else if (props.status === "failure") {
+    message = "Error: Data fetch failed";
+  } else {
+    message = "Fatal error";
+  }
 
+  if (props.reason === "loading") {
+    reply = "Searching for content";
+  } else if (props.reason === "success") {
+    reply = "Content found";
+  } else if (props.reason === "failure") {
+    reply = "Interrupted while fetching content";
+  } else {
+    reply = "Content not found";
+  }
 
-
-
-
-
-console.log(message)
+  console.log(message);
 
   return (
-
-    <div style={
-        props.status === 'loading' ? {backgroundColor: 'lightgray'}
-     : props.status === 'success' ? {backgroundColor: 'lightgreen'} 
-     : props.status === 'failure' ? {backgroundColor: 'yellow'} 
-     : {backgroundColor: 'red'}
-     }>
-        <h2>Status:</h2>
-        <p>{message}</p>
+    <div
+      style={
+        props.status === "loading"
+          ? { backgroundColor: "#ddd" }
+          : props.status === "success"
+          ? { backgroundColor: "#8f8" }
+          : props.status === "failure"
+          ? { backgroundColor: "#ff5" }
+          : { backgroundColor: "#f88" }
+      }
+    >
+      <h2>Status:</h2>
+      <h3>{message}</h3>
+      <p>{reply}</p>
     </div>
-  )
-}
+  );
+};
